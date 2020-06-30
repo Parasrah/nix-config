838af405771a05acd5f07fa6defb4cd25cd9b73e
parasrah <git@parasrah.com>
2020-06-30 14:32:02 -0600

    test ipv6 support

--- cfg/wireguard/default.nix
+++ cfg/wireguard/default.nix
@@ -1,17 +1,26 @@
 id:
+let
+  wgIpv4Address = id: "192.168.10.${builtins.toString id}";
 
+  wgIpv6Address = id: "ed32:ed32:ed32:ed32::${builtins.toString id}";
+
+  peerAddresses = id: [
+    "${wgIpv4Address id}/24"
+    "${wgIpv6Address id}/64"
+  ];
+
+in
 {
   wg0 = {
-    address = [ "192.168.10.${builtins.toString id}" ];
-     privateKey = builtins.readFile ../../secrets/wireguard/privatekey;
-     listenPort = 21841;
-     peers = [
-       {
-         publicKey = builtins.readFile ../../secrets/wireguard/s_publickey;
-         endpoint = builtins.readFile ../../secrets/wireguard/address;
-         allowedIPs = [ "0.0.0.0/0" "::/0" ];
-         persistentKeepalive = 21;
-       }
-     ];
+    address = peerAddresses id;
+    privateKey = builtins.readFile ../../secrets/wireguard/privatekey;
+    peers = [
+      {
+        publicKey = builtins.readFile ../../secrets/wireguard/s_publickey;
+        endpoint = builtins.readFile ../../secrets/wireguard/address;
+        allowedIPs = [ "0.0.0.0/0" "::/0" ];
+        persistentKeepalive = 21;
+      }
+    ];
   };
 }
