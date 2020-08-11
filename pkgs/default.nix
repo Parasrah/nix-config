78855216337ccd6063bc21077596101512875f8e
parasrah <git@parasrah.com>
2020-08-11 17:03:38 -0600

    remove unused packages & update dotfiles

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -3,7 +3,4 @@ self: super:
 {
   neovim = super.callPackage ./neovim {};
   kakoune-unwrapped = super.callPackage ./kakoune {};
-  gigalixir-cli = super.callPackage ./gigalixir-cli {};
-  sqlpackage = super.callPackage ./sqlpackage {};
-  azuredatastudio = super.callPackage ./azuredatastudio {};
 }
